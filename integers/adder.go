0dc0cd24fcdaa0bc7300629b756146fed41f7128
Alex Guidi <aguidi@tfd.ie>
2020-10-27 19:53:25 +0000

    Addind Examples

--- integers/adder.go
+++ integers/adder.go
@@ -1,6 +1,15 @@
+/*
+https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/integers
+
+What we learn:
+- More practice of the TDD workflow
+- Integers, addition
+- Writing better documentation so users of our code can understand its usage quickly
+- Examples of how to use our code, which are checked as part of our tests*/
+
 package integers
 
-//Add func
+//Add takes two integers and returns the sum of them.
 func Add(x, y int) int {
 	return x + y
 }
